03dd8a7a5b37b3c7884114f8c513887db13a923b
mitrius99 <mitrius99o@gmail.com>
2020-08-27 23:06:10 +0300

    Made CostsLib for work with DB and app

--- CostApp/Form1.cs
+++ CostApp/Form1.cs
@@ -9,64 +9,28 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.Sql;
 using System.Data.SqlClient;
+using CostLib;
 
 namespace CostApp
 {
     public partial class Form1 : Form
     {
-        SqlConnection sqlConnection;
-        public int monthCosts=0;
-        public int weekCosts=0;
-        public int dayCosts=0;
+        //SqlConnection sqlConnection;
         public Form1()
         {
             InitializeComponent();
         }
-        private async void Form1_Load(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
             string connectionString = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = C:\Users\Dmitry\source\repos\CostApp\CostApp\Database.mdf; Integrated Security = True";
-            sqlConnection = new SqlConnection(connectionString);
-            await sqlConnection.OpenAsync();
+            CostsDB.OpenSqlConnection(connectionString);
 
-            SqlDataReader sqlReader = null;
-
-            SqlCommand command = new SqlCommand("SELECT * FROM [Costs]", sqlConnection);
-            try
-            {
-                sqlReader = await command.ExecuteReaderAsync();
-
-                
-                while (await sqlReader.ReadAsync())
-                {
-                    listBox1.Items.Add(sqlReader["DateTime"] + "  " + Convert.ToString(sqlReader["Category"]) + "  " + Convert.ToString(sqlReader["Cost"]));
-                    
-                    if (DateTime.Now.Month == Convert.ToDateTime(sqlReader["DateTime"]).Month)
-                    {
-                        monthCosts += Convert.ToInt32(sqlReader["Cost"]);
-                    }
-                    if (DateTime.Now.Day == Convert.ToDateTime(sqlReader["DateTime"]).Day)
-                    {
-                        dayCosts += Convert.ToInt32(sqlReader["Cost"]);
-                    }
-                }
-                label8.Text = dayCosts.ToString();
-                label10.Text = monthCosts.ToString();
-            }
-            catch(Exception ex)
-            {
-                MessageBox.Show(ex.Message.ToString(), ex.Source.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            finally
-            {
-                if (sqlReader != null)
-                    sqlReader.Close();
-            }
-
+            CostsDB.SqlCmdView("SELECT * FROM [Costs]", listBox1, label8, label10);
         }
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
-            if (sqlConnection != null && sqlConnection.State != ConnectionState.Closed)
-                sqlConnection.Close();
+            if (CostsDB.sqlConnection != null && CostsDB.sqlConnection.State != ConnectionState.Closed)
+                CostsDB.sqlConnection.Close();
         }
 
         private async void button1_Click(object sender, EventArgs e)
@@ -76,7 +40,7 @@ namespace CostApp
                 label3.Visible = false;
             if(!string.IsNullOrEmpty(textBox1.Text)&&!string.IsNullOrEmpty(textBox2.Text))
             {
-                command = new SqlCommand("INSERT INTO [Costs] (DateTime, Category, Cost)VALUES(@DateTime, @Category, @Cost)", sqlConnection);
+                command = new SqlCommand("INSERT INTO [Costs] (DateTime, Category, Cost)VALUES(@DateTime, @Category, @Cost)", CostsDB.sqlConnection);
                 command.Parameters.AddWithValue("DateTime", DateTime.Now);
                 command.Parameters.AddWithValue("Category", textBox1.Text);
                 command.Parameters.AddWithValue("Cost", textBox2.Text);
@@ -91,95 +55,29 @@ namespace CostApp
             
         }
 
-        private async void tabControl1_Click(object sender, EventArgs e)
+        private void tabControl1_Click(object sender, EventArgs e)
         {
             listBox1.Items.Clear();
-            SqlDataReader sqlReader = null;
-
-            SqlCommand command = new SqlCommand("SELECT * FROM [Costs]", sqlConnection);
-            try
-            {
-                sqlReader = await command.ExecuteReaderAsync();
-
-                monthCosts = 0;
-                dayCosts = 0;
-
-                while (await sqlReader.ReadAsync())
-                {
-                    listBox1.Items.Add(sqlReader["DateTime"] + "  " + Convert.ToString(sqlReader["Category"]) + "  " + Convert.ToString(sqlReader["Cost"]));
-                    if (DateTime.Now.Month == Convert.ToDateTime(sqlReader["DateTime"]).Month)
-                    {
-                        monthCosts += Convert.ToInt32(sqlReader["Cost"]);
-                    }
-                    if (DateTime.Now.Day == Convert.ToDateTime(sqlReader["DateTime"]).Day)
-                    {
-                        dayCosts += Convert.ToInt32(sqlReader["Cost"]);
-                    }
-                }
-                label8.Text = dayCosts.ToString();
-                label10.Text = monthCosts.ToString();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message.ToString(), ex.Source.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            finally
-            {
-                if (sqlReader != null)
-                    sqlReader.Close();
-            }
+            CostsDB.SqlCmdView("SELECT * FROM [Costs]", listBox1, label8, label10);
         }
 
         private async void button2_Click(object sender, EventArgs e)
         {
-            SqlCommand command = new SqlCommand("TRUNCATE TABLE [Costs] ", sqlConnection);
+            SqlCommand command = new SqlCommand("TRUNCATE TABLE [Costs] ", CostsDB.sqlConnection);
             await command.ExecuteNonQueryAsync();
             listBox1.Items.Clear();
 
-            monthCosts = 0;
-            dayCosts = 0;
+            CostsDB.monthCosts = 0;
+            CostsDB.dayCosts = 0;
             label8.Text = Convert.ToString(0);
             label10.Text = Convert.ToString(0);
         }
 
-        private async void обновитьToolStripMenuItem_Click(object sender, EventArgs e)
+        private void обновитьToolStripMenuItem_Click(object sender, EventArgs e)
         {
 
             listBox1.Items.Clear();
-            SqlDataReader sqlReader = null;
-            SqlCommand command = new SqlCommand("SELECT * FROM [Costs]", sqlConnection);
-            try
-            {
-                sqlReader = await command.ExecuteReaderAsync();
-
-                monthCosts = 0;
-                dayCosts = 0;
-                while (await sqlReader.ReadAsync())
-                {
-                    listBox1.Items.Add(sqlReader["DateTime"] + "  " + Convert.ToString(sqlReader["Category"]) + "  " + Convert.ToString(sqlReader["Cost"]));
-                    if (DateTime.Now.Month == Convert.ToDateTime(sqlReader["DateTime"]).Month)
-                    {
-                        monthCosts += Convert.ToInt32(sqlReader["Cost"]);
-                    }
-                    if (DateTime.Now.Day == Convert.ToDateTime(sqlReader["DateTime"]).Day)
-                    {
-                        dayCosts += Convert.ToInt32(sqlReader["Cost"]);
-                    }
-                }
-                label8.Text = dayCosts.ToString();
-                label10.Text = monthCosts.ToString();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message.ToString(), ex.Source.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            finally
-            {
-                if (sqlReader != null)
-                    sqlReader.Close();
-            }
-
-            
+            CostsDB.SqlCmdView("SELECT * FROM [Costs]", listBox1, label8, label10);
         }
     }
 }
---
+++ CostsDB/CostsDB.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace CostLib
+{
+    public static class CostsDB
+    {
+        public static SqlConnection sqlConnection;
+        public static SqlDataReader sqlReader;
+        public static SqlCommand sqlCommand;
+
+        public static int monthCosts = 0;
+        public static int dayCosts = 0;
+        public async static void OpenSqlConnection(string connStr)
+        {
+            sqlConnection = new SqlConnection(connStr);
+            await sqlConnection.OpenAsync();
+        }
+        public async static void SqlCmdView(string request, ListBox lsCosts, Label day, Label month)
+        {
+            sqlReader = null;
+            sqlCommand = new SqlCommand(request, sqlConnection);
+            try
+            {
+                sqlReader = await sqlCommand.ExecuteReaderAsync();
+
+                monthCosts = 0;
+                dayCosts = 0;
+
+                while (await sqlReader.ReadAsync())
+                {
+                    lsCosts.Items.Add(sqlReader["DateTime"] + "  " + Convert.ToString(sqlReader["Category"]) + "  " + Convert.ToString(sqlReader["Cost"]));
+                    DayMonthAddCost();
+                }
+                day.Text = dayCosts.ToString();
+                month.Text = monthCosts.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), ex.Source.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                if (sqlReader != null)
+                    sqlReader.Close();
+            }
+        }
+        private static void DayMonthAddCost()
+        {
+            if (DateTime.Now.Month == Convert.ToDateTime(sqlReader["DateTime"]).Month)
+            {
+                monthCosts += Convert.ToInt32(sqlReader["Cost"]);
+            }
+            if (DateTime.Now.Day == Convert.ToDateTime(sqlReader["DateTime"]).Day)
+            {
+                dayCosts += Convert.ToInt32(sqlReader["Cost"]);
+            }
+        }
+        private static void ViewCostInfo(Label day, Label month)
+        {
+            day.Text = dayCosts.ToString();
+            month.Text = monthCosts.ToString();
+        }
+    }
+}
